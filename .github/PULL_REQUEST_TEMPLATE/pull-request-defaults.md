514e6d22691e21948fea9f35dc0e2faf0556e394
Alice Roherty <alice.roherty@populusgs.com>
2025-04-12 13:44:51 -0400

    Add issue and pull request templates, configure default settings, and update homepage layout with new components and theme.

---
+++ .github/PULL_REQUEST_TEMPLATE/pull-request-defaults.md
@@ -0,0 +1,10 @@
+---
+name: 'Pull Request Defaults'
+title: '<Enter a Descriptive Title>'
+assignees: 'aliceroherty'
+project: 'aliceroherty/1'
+
+---
+
+### What's Changed?
+<!-- A concise description of what you're experiencing. -->
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-	return <></>
+import './App.css'
+import 'animate.css'
+import Nav from './components/Nav'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import Experience from './components/Experience'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const muiTheme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: {
+			main: '#f9a8d4',
+		},
+		secondary: {
+			main: '#db2777',
+		},
+		error: {
+			main: '#ff0081',
+		},
+	},
+})
+
+const Home = () => {
+	return (
+		<ThemeProvider theme={muiTheme}>
+			<Nav />
+			<div className='select-none px-5 md:px-24'>
+				<Home />
+				<About />
+				<Experience />
+				<Projects />
+				<Contact />
+			</div>
+		</ThemeProvider>
+	)
 }
+
+export default Home
