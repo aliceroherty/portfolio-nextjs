f3f4d5468c2be0e897b68d6112ea72cf1bff9f23
Alice Roherty <alice.roherty@populusgs.com>
2025-05-16 18:03:23 -0400

    Refactor HamburgerMenu component and update CSS for improved positioning of links rename classes for improved clarity

--- src/app/components/controls/HamburgerMenu.tsx
+++ src/app/components/controls/HamburgerMenu.tsx
@@ -4,36 +4,38 @@ import NavItems from '../NavItems'
 
 const HamburgerMenu = () => {
     const [open, setOpen] = useState(false)
+    const handleClick = () => {
+		setOpen(!open)
+	}
 
-    return (
+	return (
 		<section className='flex lg:hidden'>
 			<button
-				className='space-y-2 [&>*]:hover:bg-gray-400 cursor-pointer [&>*]:transition-colors [&>*]:ease-in-out [&>*]:duration-300'
-				onClick={() => setOpen(prev => !prev)}
+				onClick={handleClick}
+				className='flex flex-col justify-center items-center z-20'
 			>
-				<span className='block h-0.5 w-8 bg-white'></span>
-				<span className='block h-0.5 w-8 bg-white'></span>
-				<span className='block h-0.5 w-8 bg-white'></span>
+				<span
+					className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+						open ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
+					}`}
+				></span>
+				<span
+					className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+						open ? 'opacity-0' : 'opacity-100'
+					}`}
+				></span>
+				<span
+					className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+						open ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
+					}`}
+				></span>
 			</button>
 
-			<div className={open ? 'showNav' : 'hideNav'}>
-				<button
-					className='absolute top-0 right-0 px-5 py-5 md:px-24'
-					onClick={() => setOpen(false)}
-				>
-					<svg
-						className='h-8 w-8 text-white hover:text-gray-400 transition-colors ease-in-out duration-300 cursor-pointer'
-						viewBox='0 0 24 24'
-						fill='none'
-						stroke='currentColor'
-						strokeWidth='2'
-						strokeLinecap='round'
-						strokeLinejoin='round'
-					>
-						<line x1='18' y1='6' x2='6' y2='18' />
-						<line x1='6' y1='6' x2='18' y2='18' />
-					</svg>
-				</button>
+			<div
+				className={
+					open ? 'hamburger-menu-expand' : 'hamburger-menu-collapse'
+				}
+			>
 				<ul className='flex flex-col items-center justify-between min-h-[250px]'>
 					<NavItems />
 				</ul>
